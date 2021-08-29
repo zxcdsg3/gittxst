2629311f4c657f946779829179a9b3cf946a07f3
zxcdsg3 <89727227+zxcdsg3@users.noreply.github.com>
2021-08-29 22:50:54 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello git 4!");
         System.out.println("hello git 5!");
         System.out.println("hot_fix text!");
+        System.out.println("pull text!");
     }
 }
